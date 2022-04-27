980315fdd5bba7a548360f1088212d831cd38119
Gustavo Boaz <gustavo.boaz@hotmail.com>
2022-04-27 11:31:11 -0300

    @GustavoBoaz [Task: Commit inicial] {27/04/2022 11:30}

---
+++ BlogPessoalTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlogPessoalTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
