c9a2e7c40ab727266a2528c53c81830aa2d02ca1
Gustavo Boaz <gustavo.boaz@hotmail.com>
2022-04-27 17:02:29 -0300

    @GustavoBoaz [Task: CONFIGURAÇÃO string de conexão] {27/04/2022 17:02}

--- BlogPessoal/Startup.cs
+++ BlogPessoal/Startup.cs
@@ -24,6 +24,11 @@ namespace BlogPessoal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
 
             services.AddControllers();
         }
