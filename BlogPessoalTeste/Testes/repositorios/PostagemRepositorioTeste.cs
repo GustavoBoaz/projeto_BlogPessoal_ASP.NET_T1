ff2e51ed33d46f60f5192a9bd0757c52882f0f44
Rodrigo F. (NotBuer) <rodrigosoocer@gmail.com>
2022-05-04 09:22:53 -0300

    @NotBuer [Task: Adicionado testes de tema e postagem] {04/05/2022 09:22}

---
+++ BlogPessoalTeste/Testes/repositorios/PostagemRepositorioTeste.cs
@@ -0,0 +1,136 @@
+﻿using System.Linq;
+using BlogPessoal.src.data;
+using BlogPessoal.src.dtos;
+using BlogPessoal.src.repositorios;
+using BlogPessoal.src.repositorios.implementacoes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BlogPessoalTeste.Testes.repositorios
+{
+
+    [TestClass]
+    public class PostagemRepositorioTeste
+    {
+        private BlogPessoalContexto _contexto;
+        private IUsuario _repositorioU;
+        private ITema _repositorioT;
+        private IPostagem _repositorioP;
+
+        [TestInitialize]
+        public void ConfiguracaoInicial()
+        {
+            DbContextOptions<BlogPessoalContexto> opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+            .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+            .Options;
+            _contexto = new BlogPessoalContexto(opt);
+            _repositorioU = new UsuarioRepositorio(_contexto);
+            _repositorioT = new TemaRepositorio(_contexto);
+            _repositorioP = new PostagemRepositorio(_contexto);
+            _repositorioU.NovoUsuario(
+                new NovoUsuarioDTO(
+                "Gustavo Boaz",
+                "gustavo@email.com",
+                "134652", "URLDAFOTO"
+                )
+            );
+            _repositorioU.NovoUsuario(
+                new NovoUsuarioDTO(
+                "Catarina Boaz",
+                "catarina@email.com",
+                "134652",
+                "URLDAFOTO"
+                )
+            );
+            _repositorioT.NovoTema(new NovoTemaDTO("C#"));
+            _repositorioT.NovoTema(new NovoTemaDTO("Java"));
+            _repositorioT.NovoTema(new NovoTemaDTO("Python"));
+            _repositorioT.NovoTema(new NovoTemaDTO("JavaScript"));
+        }
+
+        [TestMethod]
+        public void CriaTresPostagemNoSistemaRetornaTres()
+        {
+            // GIVEN - Dado que eu tenho 3 postagens no sistema
+            _repositorioP.NovaPostagem(
+                new NovaPostagemDTO(
+                "C# é muito massa",
+                "É uma linguagem muito utilizada no mundo",
+                "URLDAFOTO",
+                "gustavo@email.com",
+                "C#"
+                )
+            );
+            _repositorioP.NovaPostagem(
+                new NovaPostagemDTO(
+                "C# pode ser usado com Testes",
+                "O teste unitário é importante para o desenvolvimento",
+                "URLDAFOTO",
+                "catarina@email.com",
+                "C#"
+                )
+            );
+            _repositorioP.NovaPostagem(
+                new NovaPostagemDTO(
+                "Java é muito massa",
+                "Java também é muito utilizada no mundo",
+                "URLDAFOTO",
+                "gustavo@email.com",
+                "Java"
+                )
+            );
+            
+            // WHEN - Quando eu busco todas as postagens
+            // THEN - Eu tenho 3 postagens
+
+            Assert.AreEqual(3, _repositorioP.PegarTodasPostagens().Count());
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        public void AtualizarPostagemRetornaPostagemAtualizada(int idPostagem)
+        {
+            // GIVEN - Dado que eu vou atualizar a postagem de id 2
+            _repositorioP.AtualizarPostagem(
+                new AtualizarPostagemDTO(
+                /*idPostagem,*/
+                "JavaScript é muito massa",
+                "JavaScript é muito utilizada no mundo",
+                "URLDAFOTOATUALIZADA",
+                "JavaScript"
+                )
+            );
+
+            // WHEN - Quando eu busco a postagem de id 2
+            // THEN - Eu tenho a postagem atualizada
+            Assert.AreEqual(
+                "JavaScript é muito massa",
+                _repositorioP.PegarPostagemPeloId(idPostagem).Titulo
+            );
+            Assert.AreEqual(
+                "JavaScript é muito utilizada no mundo",
+                _repositorioP.PegarPostagemPeloId(idPostagem).Descricao
+            );
+            Assert.AreEqual(
+                "URLDAFOTOATUALIZADA",
+                _repositorioP.PegarPostagemPeloId(idPostagem).Foto
+            );
+            Assert.AreEqual(
+                "JavaScript",
+                _repositorioP.PegarPostagemPeloId(idPostagem).Tema.Descricao
+            );
+        }
+
+        [TestMethod]
+        [DataRow("massa", null, null)]
+        public void PegarPostagensPorPesquisaRetodarCustomizada(string titulo, string descricaoTema, string nomeCriador )
+        {
+            // WHEN - Quando eu busco as postagens pelo titulo,
+            // descricao do tema e nome do criador
+            var postagens = _repositorioP
+            .PegarPostagensPorPesquisa(titulo, descricaoTema, nomeCriador);
+            // THEN - Eu tenho as postagens que correspondem aos criterios
+            Assert.AreEqual(3, postagens.Count());
+        }
+
+    }
+}
---
+++ BlogPessoalTeste/Testes/repositorios/TemaRepositorioTeste.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using BlogPessoal.src.data;
+using BlogPessoal.src.dtos;
+using BlogPessoal.src.repositorios;
+using BlogPessoal.src.repositorios.implementacoes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BlogPessoalTeste.Testes.repositorios
+{
+    
+    [TestClass]
+    public class TemaRepositorioTeste
+    {
+
+        private BlogPessoalContexto _contexto;
+        private ITema _repositorio;
+        
+        [TestInitialize]
+        public void ConfiguracaoInicial()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+            .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+            .Options;
+            _contexto = new BlogPessoalContexto(opt);
+            _repositorio = new TemaRepositorio(_contexto);
+        }
+        
+        [TestMethod]
+        public void CriarQuatroTemasNoBancoRetornaQuatroTemas2()
+        {
+            //GIVEN - Dado que registro 4 temas no banco
+            _repositorio.NovoTema(new NovoTemaDTO("C#"));
+            _repositorio.NovoTema(new NovoTemaDTO("Java"));
+            _repositorio.NovoTema(new NovoTemaDTO("Python"));
+            _repositorio.NovoTema(new NovoTemaDTO("JavaScript"));
+            //THEN - Entao deve retornar 4 temas
+            Assert.AreEqual(4, _repositorio.PegarTodosTemas().Count);
+        }
+        
+        [TestMethod]
+        [DataRow(1)]
+        public void PegarTemaPeloIdRetornaTema1(int id)
+        {
+            //GIVEN - Dado que pesquiso pelo id 1
+            var tema = _repositorio.PegarTemaPeloId(id);
+            //THEN - Entao deve retornar 1 tema
+            Assert.AreEqual("C#", tema.Descricao);
+        }
+        
+        [TestMethod]
+        [DataRow("Java")]
+        public void PegaTemaPelaDescricaoRetornadoisTemas(string descricao)
+        {
+            //GIVEN - Dado que pesquiso pela descricao Java
+            var temas = _repositorio.PegarTemasPelaDescricao(descricao);
+            //THEN - Entao deve retornar 2 temas
+            Assert.AreEqual(2, temas.Count);
+        }
+        
+        [TestMethod]
+        [DataRow(3)]
+        public void AlterarTemaPythonRetornaTemaCobol(int id)
+        {
+            //GIVEN - Dado que passo o Id 3 e o tema COBOL
+            _repositorio.AtualizarTema(new AtualizarTemaDTO(id, "COBOL"));
+            //THEN - Entao deve retornar o tema COBOL
+            Assert.AreEqual("COBOL",
+            _repositorio.PegarTemaPeloId(id).Descricao);
+        }
+        
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        public void DeletarTemasRetornaNulo(int id)
+        {
+            //GIVEN - Dado que passo o Id 1, 2, 3, 4
+            _repositorio.DeletarTema(id);
+            //THEN - Entao deve retornar nulo
+
+            Assert.IsNull(_repositorio.PegarTemaPeloId(id));
+        }
+    }
+}
